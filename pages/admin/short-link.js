a15c351fb9ea91cb41b1fe45c7346e97dc6fdabc
justpiple <me@masben.studio>
2022-05-07 23:06:05 +0700

    auto generate short id

--- pages/admin/short-link.js
+++ pages/admin/short-link.js
@@ -109,7 +109,7 @@ function BlogAdmin({ links }) {
                                 <label className="block text-white text-left" htmlFor="id">
                                     Short id
                                 </label>
-                                <input id="id" autoComplete="off" name="newId" className="w-full bg-gray-200 appearance-none border-2 border-gray-200 rounded py-2 px-4 text-gray-700 leading-tight focus:outline-none focus:bg-white focus:border-gray-400" type="text" required />
+                                <input id="id" autoComplete="off" name="newId" className="w-full bg-gray-200 appearance-none border-2 border-gray-200 rounded py-2 px-4 text-gray-700 leading-tight focus:outline-none focus:bg-white focus:border-gray-400" type="text" />
                             </div>
                             <div className="flex items-center p-6 space-x-2 rounded-b border-t border-gray-200 dark:border-gray-600">
                                 <button type="submit" className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Submit</button>
