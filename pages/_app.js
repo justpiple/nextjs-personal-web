d4980a5e470e07e604c0b8410e7e8a2207c6add9
justpiple <me@itsben.space>
2022-06-27 18:23:47 +0700

    add Google Analytics

--- pages/_app.js
+++ pages/_app.js
@@ -1,14 +1,17 @@
 import '../styles/globals.css';
 import NextNProgress from 'nextjs-progressbar';
+import { GoogleAnalytics, usePageViews } from "nextjs-google-analytics";
 import Head from 'next/head';
 
 function MyApp({ Component, pageProps }) {
+  usePageViews();
   return <>
     <Head>
       <link rel="preconnect" href="https://fonts.gstatic.com" />
       <link href="https://fonts.googleapis.com/css2?family=Inter&family=Josefin+Sans&display=swap" rel="stylesheet" />
       <link rel="manifest" href="/manifest.json" />
     </Head>
+    <GoogleAnalytics />
     <NextNProgress options={{ easing: "ease", speed: 500 }} color="#74f2d8" />
     <Component {...pageProps} />
   </>
