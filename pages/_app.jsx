48186ffa1b8287103224a18922d814ddfe2d3ea4
justpiple <me@itsben.space>
2023-03-14 07:14:57 +0700

    update

--- pages/_app.jsx
+++ pages/_app.jsx
@@ -1,20 +1,25 @@
-import '../styles/globals.css';
-import NextNProgress from 'nextjs-progressbar';
+import "../styles/global.css";
+import NextNProgress from "nextjs-progressbar";
 import { GoogleAnalytics, usePageViews } from "nextjs-google-analytics";
-import Head from 'next/head';
+import Head from "next/head";
 
 function MyApp({ Component, pageProps }) {
   usePageViews();
-  return <>
-    <Head>
-      <link rel="preconnect" href="https://fonts.gstatic.com" />
-      <link href="https://fonts.googleapis.com/css2?family=Inter&family=Josefin+Sans&display=swap" rel="stylesheet" />
-      <link rel="manifest" href="/manifest.json" />
-    </Head>
-    <GoogleAnalytics />
-    <NextNProgress options={{ easing: "ease", speed: 500 }} color="#74f2d8" />
-    <Component {...pageProps} />
-  </>
+  return (
+    <>
+      <Head>
+        <link rel="preconnect" href="https://fonts.gstatic.com" />
+        <link
+          href="https://fonts.googleapis.com/css2?family=Inter&family=Josefin+Sans&display=swap"
+          rel="stylesheet"
+        />
+        <link rel="manifest" href="/manifest.json" />
+      </Head>
+      <GoogleAnalytics />
+      <NextNProgress options={{ easing: "ease", speed: 500 }} color="#74f2d8" />
+      <Component {...pageProps} />
+    </>
+  );
 }
 
-export default MyApp
+export default MyApp;
